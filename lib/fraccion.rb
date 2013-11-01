27a1099d7acee32d4295d6ad20f740c83b9728bc
Sem Ramos Herrera <alu0100603194@ull.edu.es>
2013-11-01 17:00:38 +0000

    Resubiendo ficheros correctos de la practica anterior

--- lib/fraccion.rb
+++ lib/fraccion.rb
@@ -1,138 +1,163 @@
 require "./lib/gcd.rb"
 
+#############################################################
+# Clase Fraccion de la practica #6 de LPP                   #
+#############################################################
+
 class Fraccion 
   
     attr_reader :numer, :denomin
   
-   def initialize(n,d) 
-    @numer, @denomin = n, d
-   end
-  
-   
+    def initialize(n,d) 
+      @numer, @denomin = n, d
+    end
+	
+	#############################################################
+    #  Funciones basicas de la clase                            #
+    #############################################################
+    
+	# Getter del num
     def num()
-      return @numer 											#Getter del num
+      return @numer
     end
 
+	# Getter del denom
     def denom()
-      return @denomin											#Getter del denom
+      return @denomin
     end
   
+	# Funcion para simplificar y mostrar la fraccion en formato reducido
     def simplificar
-      aux = gcd(self.numer, self.denomin)                                                        #MCD entre num y denom
-      Fraccion.new(self.numer/aux, self.denomin/aux) 
-												  #Simplificamos la fraccion que le pasemos
+      aux = gcd(self.numer, self.denomin)                        # MCD entre num y denom
+      Fraccion.new(self.numer/aux, self.denomin/aux)			 # Simplificamos la fraccion que le pasemos
     end 
-   
+    
+    #Mostramos los atributos en un string
     def to_s
-      "#{@numer}/#{@denomin}"										#Mostramos los atributos en un string
+      "#{@numer}/#{@denomin}"
     end
    
+	#Mostramos los atributos en float
     def to_f
-      @numer/@denomin											#Mostramos los atributos en float
+      @numer/@denomin.to_f
     end
    
     #############################################################
+    #  Funciones de igualdad, absoluto, reciproco y opuesto     #
+    #############################################################
     
-   def ==(other)										#Comparamos la fraccion implicita, con la que le pasamos como parametro   
-    other.simplificar
-    self.simplificar
-      if ((@numer == other.numer) and (@denomin == other.denomin))					#Si el numerador y el denominador de la implicita coinciden con los 
-	  true
-		#puts "Las fracciones #{self.to_s} y #{other.to_s} son iguales"				#de la pasada por parametro, entonces las fracciones son iguales
-      else 											#Si no, son distintas.
-	    false
-		#puts "Las fracciones #{self.to_s} y #{other.to_s} son distintas"
-      end  
+    # Funcion de comparar igualdad: Comparamos la fraccion implicita, con la que le pasamos como parametro
+    def ==(other)
+		other.simplificar
+		self.simplificar
+		if ((@numer == other.numer) and (@denomin == other.denomin))
+			true		# Si el numerador y el denominador de la implicita coinciden con los 
+						# de la pasada por parametro, entonces las fracciones son iguales (true)
+		else
+			false		# Si no, son distintas (false)
+		end  
     end
   
-  
-  def absoluto											#Sobrecargamos la funcion Abs para que pueda comparar fracciones
-    @numer = @numer.abs
-    @denomin = @denomin.abs
-   # Fraccion.new(n,d)
-  end
-  
-  
-  def reciprocal										#Obtiene la fraccion reciproca a la implicita
-    aux = self.num()
-    numer = self.denom()
-    denomin = aux
-  end
-  
-  
-  def opuesto
-    @numer = -@numer
-  end
-  
-  ################################################################################
-  
-  def mcm(n, m)                                                                              	#Necesitamos hallar el mcm para la suma y la resta
-    aux=gcd(n,m)
-    (n/aux)*m 
-  end
-
-  def /(other) 				                                                       #Realizamos la division de la fraccion implicita con recibida como parametro                                                                                
-    temp = Fraccion.new(@numer*other.denomin, @denomin*other.numer)
-    temp.simplificar
-  end
-  
-  def +(other)                                                                     		#Realizamos la suma de la fraccion implicita con recibida como parametro          
-    aux = mcm(@denomin, other.denomin)                                                    
-    temp = Fraccion.new(((aux/@denomin)*@numer)+((aux/other.denomin)*other.numer),aux)               
-    temp.simplificar
-  end
-        
-  def -(other)                                                                        	#Realizamos la resta de la fraccion implicita con recibida como parametro                                                                       
-    aux = mcm(@denomin, other.denomin)
-    temp = Fraccion.new(((aux/@denomin)*@numer)-((aux/other.denomin)*other.numer),aux)
-    temp.simplificar                                 
-  end
-   
-  def *(other)                                                                     		#Realizamos la multiplicacion de la fraccion implicita con recibida como parametro                                                                                
-   temp=Fraccion.new(@numer*other.numer, @denomin*other.denomin) 
-   temp.simplificar                                                
-  end 
-               
-  
-   
-  
-  ##########################################################
-  
-   def %(other)											#Obtenemos el resto de la division de dos fracciones								
-      aux1 = self.numer * other.denomin								#Multiplicamos el numerador de la primera entre el denominador de la segunda
-      aux2 = self.denomin * other.numer								#Multiplicamos el denominador de la primera entre el numerador de la segunda
-      resto = aux1%aux2										#Obtenemos el Resto de la fraccion resultante
-   end  
-  
-   def <(other)											#Comprobamos que la matriz implicita es menor que la recibida.
-      if (self.numer/self.denomin) < (other.numer/other.denomin) 
-	true  
-      else
-	false				
-      end
-   end
-    
-   def >(other)
-    if((self.numer/self.denomin)<(other.numer/other.denomin))						#Comprobamos que la matriz implicita es mayor que la recibida.
-      false				
-    else
-      true				
-    end
-   end
- 
-   def <=(other)											#Comprobamos que la matriz implicita es menor o igual que la recibida.
-      if((self.numer/self.denomin)<=(other.numer/other.denomin))
-	true				
-      else  
-	false
-      end
-    end
- 
-    def >=(other)											#Comprobamos que la matriz implicita es mayor o igual que la recibida.
-	if((self.numer/self.denomin)>=(other.numer/other.denomin))
-	  true
-	else 
-	  false
+	# Funcion que devuelve la funcion en valor absoluto
+	def absoluto
+		@numer = @numer.abs
+		@denomin = @denomin.abs
+		# Fraccion.new(n,d)
 	end
+  
+	#Obtiene la fraccion reciproca a la implicita
+	def reciprocal
+		aux = @numer
+		@numer = @denomin
+		@denomin = aux
+	end
+  
+	# Funcion que calcula la fraccion opuesta
+	def opuesto
+		@numer = -@numer
+	end
+  
+	#############################################################
+    #  Funciones de suma, resta, multiplicacion y division      #
+    #############################################################
+	
+	# Necesitamos hallar el minimo comun multiplo para la suma y la resta
+	def mcm(n, m)
+		aux=gcd(n,m)
+		(n/aux)*m 
+	end
+	
+	# Realizamos la division de la fraccion implicita con recibida como parametro
+	def /(other)
+		temp = Fraccion.new(@numer*other.denomin, @denomin*other.numer)
+		temp.simplificar
+	end
+	
+	# Realizamos la suma de la fraccion implicita con recibida como parametro
+	def +(other)                                                                     		          
+		aux = mcm(@denomin, other.denomin)                                                    
+		temp = Fraccion.new(((aux/@denomin)*@numer)+((aux/other.denomin)*other.numer),aux)               
+		temp.simplificar
+	end
+    
+    # Realizamos la resta de la fraccion implicita con recibida como parametro
+	def -(other)
+		aux = mcm(@denomin, other.denomin)
+		temp = Fraccion.new(((aux/@denomin)*@numer)-((aux/other.denomin)*other.numer),aux)
+		temp.simplificar                                 
+	end
+   
+	# Realizamos la multiplicacion de la fraccion implicita con recibida como parametro
+	def *(other)
+		temp=Fraccion.new(@numer*other.numer, @denomin*other.denomin) 
+		temp.simplificar                                                
+	end
+	
+	#############################################################
+    #  Funciones de modulo y comparacionales (menor, mayor,     #
+    #  menor o igual y mayor o igual)
+    #############################################################
+  
+	def %(other)										#Obtenemos el resto de la division de dos fracciones								
+		aux1 = self.numer * other.denomin				#Multiplicamos el numerador de la primera entre el denominador de la segunda
+		aux2 = self.denomin * other.numer				#Multiplicamos el denominador de la primera entre el numerador de la segunda
+		resto = aux1%aux2								#Obtenemos el Resto de la fraccion resultante
+	end  
+  
+	# Comprobamos si la matriz implicita es menor que la recibida
+	def <(other)
+		if (self.numer/self.denomin) < (other.numer/other.denomin) 
+			true  
+		else
+			false				
+		end
+	end
+    
+	# Comprobamos si la matriz implicita es mayor que la recibida
+	def >(other)
+		if((self.numer/self.denomin)<(other.numer/other.denomin))
+			false				
+		else
+			true
+		end
+	end
+ 
+	# Comprobamos que la matriz implicita es menor o igual que la recibida
+	def <=(other)
+		if((self.numer/self.denomin)<=(other.numer/other.denomin))
+			true				
+		else  
+			false
+		end
     end
+ 
+    # Comprobamos que la matriz implicita es mayor o igual que la recibida
+    def >=(other)
+		if((self.numer/self.denomin)>=(other.numer/other.denomin))
+			true
+		else 
+			false
+		end
+    end
+
 end
--- spec/spec_fraccion.rb
+++ spec/spec_fraccion.rb
@@ -2,129 +2,142 @@ require "./lib/fraccion.rb"
 
 
 describe Fraccion do
-  
+	
+	# Creamos dos fracciones (para sumar y comparar luego)
     before :each do
-      @frac1 = Fraccion.new(5,2)
-      @frac2 = Fraccion.new(3,6)
+      @frac1 = Fraccion.new(5,2)	# Fraccion: 5/2
+      @frac2 = Fraccion.new(3,6)	# Fraccion: 3/6 --> se reduce a 1/2
     end 
 
-    describe "# Almacenamiento de los datos" do
-    	
-	it "Debe existir un numerador de la fraccion 1" do
-		@frac1.num.should == 5
-    	end
-    	it "Debe existir un numerador de la fraccion 1" do
-		@frac1.denom.should == 2
+    describe "Almacenamiento de los datos" do
+    
+    	# Comprobamos que se han creado correctamente
+		it "Debe existir un numerador de la fraccion 1" do
+			@frac1.num.should == 5
+		end
+		it "Debe existir un numerador de la fraccion 1" do
+			@frac1.denom.should == 2
+		end
+		it "Debe existir un numerador de la fraccion 2" do
+			@frac2.num.should == 3
+		end
+		it "Debe existir un numerador de la fraccion 2" do
+			@frac2.denom.should == 6
+		end
+	
+		# Simplificamos la fraccion 2, que debe ser 1/2
+		it "Debe de estar en su forma reducida" do
+			@frac2.simplificar.numer.should eq(1)
+			@frac2.simplificar.denomin.should eq(2)
+		end
+		
+		# Comprobamos los metodos num() y denom() implementados en la clase
+		it "Se debe invocar al metodo num() para obtener el numerador" do
+			@frac1.num.should eq(5) 
+		end
+		it "Se debe invocar al metodo denom() para obtener el denominador" do
+			@frac1.denom.should eq(2)  
+		end
+		
 	end
-	it "Debe existir un numerador de la fraccion 2" do
-		@frac2.num.should == 3
-    	end
-    	it "Debe existir un numerador de la fraccion 2" do
-		@frac2.denom.should == 6
-	end    	
 	
-	#################################################################
+	describe "Comprobamos las funciones to_s y to_f" do
+		it "Se debe mostrar por la consola la fraccion de la forma: a/b, donde a es el numerador y b el denominador" do
+			@frac1.to_s.should eq("5/2")
+		end
+		it "Se debe mostrar por la consola la fraccion en formato flotante" do
+			@frac1.to_f.should == 5/2.0
+		end
+	end
+    
+    describe "Comprobamos las funciones ==, absoluto, reciproco e inverso" do
+		it "Se debe comparar si dos fracciones son iguales con ==" do
+			temp = (@frac1 == @frac2)
+			temp.should eq(false)
+		end
+    
+		it "Se debe calcular el valor absoluto de una fraccion con el metodo abs" do
+			Aux1=Fraccion.new(-5,2)
+			Aux1.absoluto
+			Aux1.numer.should eq(@frac1.numer)
+			Aux1.denomin.should eq(@frac1.denomin)
+		end
 	
-	it "Debe de estar en su forma reducida" do
-	      
-	
-		@frac2.simplificar.numer.should eq(1)
-		@frac2.simplificar.denomin.should eq(2)
+		it "Se debe calcular el reciproco de una fraccion con el metodo reciprocal" do
+			Aux=Fraccion.new(2,5)
+			Aux.reciprocal
+			Aux.numer.should eq(@frac1.numer)
+			Aux.denomin.should eq(@frac1.denomin)
+		end
 
+		it "Se debe calcular el opuesto de una fraccion con -" do
+			Aux2 = Fraccion.new(-5,2)
+			Aux2.opuesto
+			Aux2.numer.should eq(@frac1.numer)
+		end
+	
 	end
-	it "Se debe invocar al metodo num() para obtener el numerador" do
-		@frac1.num.should eq(5) 
-	end
-  
-	it "Se debe invocar al metodo denom() para obtener el denominador" do
-		@frac1.denom.should eq(2)  
-	end 
 	
-	it "Se debe mostrar por la consola la fraccion de la forma: a/b, donde a es el numerador y b el denominador" do
-	  #@frac1.should eq("5/2")  #Esta linea no funciona, debido a que me da un error como que la funcion "EQ" no existe.
-	end
-	it "Se debe mostrar por la consola la fraccion en formato flotante" do
-		@frac1.to_f.should == 5/2
-	end
-    
-    ################################################################
-    
-    it "Se debe comparar si dos fracciones son iguales con ==" do
-		temp = (@frac1 == @frac2)
-		temp.should eq(false)
-    end
-    
-    it "Se debe calcular el valor absoluto de una fraccion con el metodo abs" do
-	Aux1=Fraccion.new(-5,2)
-	Aux1.absoluto
-	Aux1.numer.should eq(@frac1.numer)
-	Aux1.denomin.should eq(@frac1.denomin)
-    end
+    describe "Comprobamos los operadores arimeticos (suma, resta, multiplicacion y division" do
 	
-    it " Se debe calcular el reciproco de una fraccion con el metodo reciprocal" do
-	Aux=Fraccion.new(2,5)
-	Aux.reciprocal
-	Aux.numer.should eq(@frac1.denomin)
-	Aux.denomin.should eq(@frac1.numer)
-    end
-
-    it "Se debe calcular el opuesto de una fraccion con -" do
-	Aux2 = Fraccion.new(-5,2)
-	Aux2.opuesto
-	Aux2.numer.should eq(@frac1.numer)
-    end
-	
-    ###################################################################
-	
-    it "Se debe sumar dos fracciones con + y dar el resultado de forma reducida" do
-	aux =	(@frac1 + @frac2)
-	
-	aux.denomin.should eq(1)
-	aux.numer.should eq(3)
-    end
+		it "Se debe sumar dos fracciones con + y dar el resultado de forma reducida" do
+			aux =	(@frac1 + @frac2)	
+			aux.denomin.should eq(1)
+			aux.numer.should eq(3)
+		end
    
-    
-    it "Se debe restar dos fracciones con - y dar el resultado de forma reducida" do
-	aux = @frac1 - @frac2
-	aux.numer.should eq(2)
-	aux.denomin.should eq(1)
-    end
+		it "Se debe restar dos fracciones con - y dar el resultado de forma reducida" do
+			aux = @frac1 - @frac2
+			aux.numer.should eq(2)
+			aux.denomin.should eq(1)
+		end
 	
-    it "Se debe multiplicar dos fracciones con * y dar el resultado de forma reducida" do
-	aux = @frac1 * @frac2
-	aux.numer.should eq(5)
-	aux.denomin.should eq(4)
-    end
+		it "Se debe multiplicar dos fracciones con * y dar el resultado de forma reducida" do
+			aux = @frac1 * @frac2
+			aux.numer.should eq(5)
+			aux.denomin.should eq(4)
+		end
     
-    it "Se debe dividir dos fracciones con / y dar el resultado de forma reducida" do
-	aux = @frac1 / @frac2
-	aux.numer.should eq(5)
-	aux.denomin.should eq(1)
-    end
+		it "Se debe dividir dos fracciones con / y dar el resultado de forma reducida" do
+			aux = @frac1 / @frac2
+			aux.numer.should eq(5)
+			aux.denomin.should eq(1)
+		end
+	
+	end
  
-    #####################################################################
+    describe "Comprobamos las funciones resto, y las operaciones logicas" do
 
-    it "Se debe calcular el resto de dos fracciones con % y dar el resultado de forma reducida" do
-	aux = @frac1%@frac2
-	aux.should eq(0)
-    end
+		it "Se debe calcular el resto de dos fracciones con % y dar el resultado de forma reducida" do
+			aux = @frac1%@frac2
+			aux.should eq(0)
+		end
     
-    it "Se debe de poder comprobar si una fracion es menor que otra" do
-	(@frac1 < Fraccion.new(1,2)).should == false
-    end
+		it "Se debe de poder comprobar si una fracion es menor que otra" do
+			(@frac1 < Fraccion.new(1,2)).should == false
+		end
 
-    it "Se debe de poder comprobar si una fracion es mayor que otra" do
-	(@frac1 > Fraccion.new(1, 1)).should == true
-    end
+		it "Se debe de poder comprobar si una fracion es mayor que otra" do
+			(@frac1 > Fraccion.new(1, 1)).should == true
+		end
     
-    it "Se debe de poder comprobar si una fracion es menor o igual que otra" do
-	(@frac1 <= Fraccion.new(1,2)).should == false
-    end
+		it "Se debe de poder comprobar si una fracion es menor o igual que otra" do
+			(@frac1 <= Fraccion.new(1,2)).should == false
+		end
     
-    it "Se debe de poder comprobar si una fracion es mayor o igual que otra" do
-	(@frac1 >= Fraccion.new(1,2)).should == true
-    end  
+		it "Se debe de poder comprobar si una fracion es mayor o igual que otra" do
+			(@frac1 >= Fraccion.new(1,2)).should == true
+		end  
     
-  end
+	end
+	
+	describe "Modificacion de clase" do
+		it "Comprobar el reciproco de una division de dos fracciones" do
+			aux = @frac1/@frac2
+			aux.reciprocal
+			aux.numer.should eq(1)
+			aux.denomin.should eq(5)
+		end
+	end
 
 end
